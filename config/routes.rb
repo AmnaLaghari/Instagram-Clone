ce6c0ce1dce857934f962c91d2276b19606e7a7e
Amna-Laghari <amna.laghari@devsinc.com>
2022-08-01 11:26:07 +0500

    Posts:
    * Created Post model, controller and views
    * Set its index page
    * added association with user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'posts/index'
+  # get 'posts/edit'
+  # get 'posts/show'
+  # get 'posts/delete'
+  # get 'posts/new'
+  resources :users do
+    resources :posts
+  end
+  # root 'home_page#index'
+  # get 'users/index'
+  # get '/users', to: 'home_page#index'
   devise_for :users
   resources :home
 
   authenticated do
-    root 'home#about'
+    root 'posts#index'
   end
 
   unauthenticated do
