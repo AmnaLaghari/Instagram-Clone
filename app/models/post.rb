08dbaae7ccd83dad980002f739ce4ba29fc9353e
Amna-Laghari <amna.laghari@devsinc.com>
2022-08-02 01:25:48 +0500

    Added designing
    * added bootstrap styling to post and user views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+  validates :images, length: { minimum: 0, maximum: 10, message: 'can not exceed 10 per post.' }
 end
