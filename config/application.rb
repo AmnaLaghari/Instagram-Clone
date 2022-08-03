c22a21df4fefbc33d3bd169fea66279c0005a99a
Amna-Laghari <amna.laghari@devsinc.com>
2022-08-03 13:32:06 +0500

    Corrected indentation and run rubocop

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module InstagramClone
     ActiveSupport.on_load(:active_record) do
       extend ActiveStorage::Attached::Macros
     end
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'mail.google.com',
-    user_name: Rails.application.credentials.dig(:username),
-    password: Rails.application.credentials.dig(:password),
+    user_name: Rails.application.credentials[:username],
+    password: Rails.application.credentials[:password],
     authentication: 'plain',
     enable_starttls_auto: true
   }
