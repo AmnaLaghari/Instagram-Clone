70fc4381544f32511ca5d23962bae6e777b73751
Amna-Laghari <amna.laghari@devsinc.com>
2022-08-08 16:57:10 +0500

    Edit comments
    * added feature to edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
   before_action :set_comment, only: %i[show edit update destroy]
 
   def index
@@ -21,12 +22,33 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+    if @comment.update(comment_params)
+      redirect_to @post, notice: 'Comment was successfully updated.'
+    else
+      render 'edit', notice: 'Something went wrong'
+    end
+  end
+
   def destroy
+    if @comment.destroy
+      redirect_to @post, notice: 'Comment was successfully deleted.'
+    else
+      redirect_to comments_url, notice: 'Something went wrong'
+    end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content, :user_id, :post_id)
+    params.require(:comment).permit(:content, :user_id, :post_id, [:comment])
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 
 end
