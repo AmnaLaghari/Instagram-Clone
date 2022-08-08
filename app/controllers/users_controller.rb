a73284251d2c9b36837aa30de302aaae7e1fa456
Amna-Laghari <amna.laghari@devsinc.com>
2022-08-08 20:35:52 +0500

    run git rebase

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
   def index
     @users = User.all
   end
 
   private
-
-  def set_user
+  def set_post
     @user = User.find(params[:id])
   end
 end
