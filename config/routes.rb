5e5d324cdb05f8dde73897104375ab50df05620c
Amna-Laghari <amna.laghari@devsinc.com>
2022-08-08 20:44:35 +0500

    stash appky

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,10 @@ Rails.application.routes.draw do
   get 'search', to: 'search#index'
   resources :posts
   devise_for :users
-<<<<<<< Updated upstream
   resources :home
   resources :users
   resource :relationships, only: %i[create destroy]
-=======
-  resources :users
 
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-  get 'home_page/index'
-  get 'home_page/about'
-
->>>>>>> Stashed changes
   authenticated do
     root 'posts#index'
   end
