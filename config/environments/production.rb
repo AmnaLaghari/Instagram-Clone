c5f201313d526f66528b61b1d7cd7b515b0741e2
Amna-Laghari <amna.laghari@devsinc.com>
2022-08-23 17:49:09 +0500

    Added ajax configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,9 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  config.serve_static_files = true
+
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
